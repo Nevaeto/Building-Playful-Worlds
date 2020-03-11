dc656a1ca1c523ea8485960754c249937a318ae2
Nevaeto <maaike.smidt@live.nl>
2020-03-11 22:43:02 +0100

    particle system tutorial

--- HowToMakeAGameTutorial/Assets/Scrips/GameManager.cs
+++ HowToMakeAGameTutorial/Assets/Scrips/GameManager.cs
@@ -7,9 +7,11 @@ public class GameManager : MonoBehaviour
 
     public float restartDelay = 1f;
 
+    public GameObject completeLevelUI;
+
     public void completeLevel ()
     {
-        Debug.Log("LEVEL WON");
+        completeLevelUI.SetActive(true);
     }
   public void EndGame ()
     {
