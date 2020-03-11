59b97c0aa4da5d340a796ab296f8dcbeffd1ef0b
Nevaeto <maaike.smidt@live.nl>
2020-03-12 00:21:13 +0100

    base own unity game

---
+++ CakeTheClown/Assets/CollisionBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CollisionBehaviour : MonoBehaviour
+{
+    // 1
+    public bool collided;
+
+    //2
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!collided)
+        {
+            collided = true;
+            // 3
+            GameObject clown = gameObject.transform.parent.gameObject;
+            Animator clownAnimator = clown.GetComponent<Animator>();
+            // 4
+            clownAnimator.SetTrigger("hit");
+        }
+    }  
+}
