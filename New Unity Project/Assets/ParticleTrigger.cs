050dbd852b5b16d20a9e7664d827a5eea9bfbb9b
Nevaeto <maaike.smidt@live.nl>
2020-03-19 02:41:40 +0100

    Particle Trigger, Menu UI, LayOut

---
+++ New Unity Project/Assets/ParticleTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ParticleTrigger : MonoBehaviour
+{
+    public Transform Sparkles;
+    public AudioClip TriggerSound;
+    AudioSource audioSource;
+
+     void Start()
+    {
+        Sparkles.GetComponent<ParticleSystem>().Stop();
+        audioSource = GetComponent<AudioSource>();
+    }
+    void OnTriggerEnter ()
+    {
+        Debug.Log ("Start Particles");
+        Sparkles.GetComponent<ParticleSystem>().Play();
+
+        if (TriggerSound != null)
+        {
+            audioSource.PlayOneShot(TriggerSound);
+        }
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/ChangeColour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ChangeColour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Pillar;
+
+    void OnCollisionEnter() 
+    {
+        var cubeRenderer = Pillar.GetComponent<Renderer>();
+        cubeRenderer.material.SetColor("_Color", Color.yellow);
+    }
+}
