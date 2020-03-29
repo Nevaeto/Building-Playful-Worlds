1ee58145f9fe4737e2aa85b0ce403aada5130a8a
Nevaeto <maaike.smidt@live.nl>
2020-03-29 20:06:47 +0200

    Eindproject Finished

---
+++ New Unity Project/Assets/Scripts/ParticleTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ParticleTrigger : MonoBehaviour
+{
+    public Transform Sparkles;
+
+
+     void Start()
+    {
+        Sparkles.GetComponent<ParticleSystem>().Stop();
+
+    }
+    void OnTriggerEnter ()
+    {
+        Debug.Log ("Start Particles");
+        Sparkles.GetComponent<ParticleSystem>().Play();
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/ToCredits.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToCredits : MonoBehaviour
+{
+    public void LoadNextLevel ()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
