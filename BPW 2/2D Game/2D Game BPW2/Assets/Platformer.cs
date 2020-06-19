dd33ae1ff707ecf3211991804707d50683f67290
Nevaeto <maaike.smidt@live.nl>
2020-06-19 12:01:20 +0200

    2D game BPW

---
+++ BPW 2/2D Game/2D Game BPW2/Assets/Platformer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platformer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Rigidbody2D rb;
+    
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+
+        float moveBy = x * speed;
+
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+    }
+}
